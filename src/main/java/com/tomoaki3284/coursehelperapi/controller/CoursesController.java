1095ab7622dffb0c577ebd6d22d93df29cd674e1
Tomoaki Mitsuhashi <tomoaki3284@gmail.com>
2021-11-26 02:24:26 -0500

    Added get mapping to CoursesController. Added header comments.

--- src/main/java/com/tomoaki3284/coursehelperapi/controller/CoursesController.java
+++ src/main/java/com/tomoaki3284/coursehelperapi/controller/CoursesController.java
@@ -3,7 +3,10 @@ package com.tomoaki3284.coursehelperapi.controller;
 import com.tomoaki3284.coursehelperapi.model.Course;
 import com.tomoaki3284.coursehelperapi.service.CoursesService;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +22,28 @@ public class CoursesController {
 		this.coursesService = coursesService;
 	}
 	
+	/**
+	 * Fetch courses by utilizing coursesService method, and return the result
+	 *
+	 * @return List<Course> list of courses
+	 * @throws InterruptedException
+	 */
+	@CrossOrigin
 	@GetMapping("/courses")
-	public List getCourses() {
-		// todo: fetch courses from service class and return it
+	public List getCourses() throws InterruptedException {
 		List<Course> courses = coursesService.getCoursesFromS3();
 		return courses;
+	}
+	
+	/**
+	 * Fetch courses, then map by course title to return it
+	 *
+	 * @return List of courses group by course title
+	 */
+	@CrossOrigin
+	@GetMapping("/courses/group-by-title")
+	public Map<String,List<Course>> getCourseGroupByTitle() {
+		List<Course> courses = coursesService.getCoursesFromS3();
+		return courses.stream().collect(Collectors.groupingBy(Course::getTitle));
 	}
 }
