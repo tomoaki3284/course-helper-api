ca3f6361819bf8be104bc1fbd401a5bce1df9ae1
Tomoaki Mitsuhashi <tomoaki3284@gmail.com>
2021-12-14 11:43:36 -0500

    implemented local cache

--- src/main/java/com/tomoaki3284/coursehelperapi/controller/CoursesController.java
+++ src/main/java/com/tomoaki3284/coursehelperapi/controller/CoursesController.java
@@ -4,6 +4,7 @@ import com.tomoaki3284.coursehelperapi.model.Course;
 import com.tomoaki3284.coursehelperapi.service.CoursesService;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -31,7 +32,7 @@ public class CoursesController {
 	@CrossOrigin
 	@GetMapping("/courses")
 	public List getCourses() throws InterruptedException {
-		List<Course> courses = coursesService.getCoursesFromS3();
+		List<Course> courses = coursesService.getCourses();
 		return courses;
 	}
 	
@@ -43,7 +44,9 @@ public class CoursesController {
 	@CrossOrigin
 	@GetMapping("/courses/group-by-title")
 	public Map<String,List<Course>> getCourseGroupByTitle() {
-		List<Course> courses = coursesService.getCoursesFromS3();
-		return courses.stream().collect(Collectors.groupingBy(Course::getTitle));
+		List<Course> courses = coursesService.getCourses();
+		Map<String, List<Course>> map = courses.stream().collect(Collectors.groupingBy(Course::getTitle));
+		map = new TreeMap<>(map);
+		return map;
 	}
 }
--- src/main/java/com/tomoaki3284/coursehelperapi/service/CoursesService.java
+++ src/main/java/com/tomoaki3284/coursehelperapi/service/CoursesService.java
@@ -2,22 +2,40 @@ package com.tomoaki3284.coursehelperapi.service;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.tomoaki3284.coursehelperapi.model.Course;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CoursesService {
+	// this cache's key is redundant since uri is fixed
+	// key = uri, val = List of posts that can be fetch from the uri
+	// if cache doesn't exist, simply fetch
+	private final LoadingCache<String, List<Course>> coursesCache;
 	
-	private final String s3URL = "https://coursehelper.s3.amazonaws.com/current-semester.json";
+	public CoursesService() {
+		coursesCache = Caffeine.newBuilder()
+			.expireAfterWrite(3, TimeUnit.DAYS)
+			.build(uri -> getCoursesFromS3(uri));
+	}
 	
-	public List<Course> getCoursesFromS3() {
+	public List<Course> getCourses() {
+		final String s3URL = "https://coursehelper.s3.amazonaws.com/current-semester.json";
+		return coursesCache.get(s3URL);
+	}
+	
+	public List<Course> getCoursesFromS3(final String uri) {
 		ObjectMapper mapper = new ObjectMapper();
 		List<Course> courses = new ArrayList<>();
 		try {
-			URL url = new URL(s3URL);
-			courses = mapper.readValue(url, new TypeReference<List<Course>>() {});
+			URL url = new URL(uri);
+			courses = mapper.readValue(url, new TypeReference<ArrayList<Course>>() {});
 			if (courses == null) {
 				System.out.println("course is null");
 			}
